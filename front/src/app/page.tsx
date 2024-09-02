18b79c295887d883b01f006be8ddef3e6f02fefc
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-09-02 19:00:47 +0530

    Google Login setup, Login Modal, Custom Type in Next Auth

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,29 @@
+import FeatureSection from "@/components/base/FeatureSection";
+import Footer from "@/components/base/Footer";
+import HeroSection from "@/components/base/HeroSection";
+import Navbar from "@/components/base/Navbar";
+import UserReviews from "@/components/base/UserReviews";
+import { getServerSession } from "next-auth";
+import { authOption, CustomSession } from "./api/auth/[...nextauth]/options";
 
 
-export default function Home() {
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOption);
   return (
-    <h1>Hello Bro</h1>
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar user={session?.user} />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
